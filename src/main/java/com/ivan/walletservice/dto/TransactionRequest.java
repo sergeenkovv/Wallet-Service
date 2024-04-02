a37ed576934cba24089edce722496bdb94e41766
ssssssergeenkov <itproger181920@gmail.com>
2024-04-03 02:48:16 +0300

    fixed the bug

--- src/main/java/com/ivan/walletservice/dto/TransactionRequest.java
+++ src/main/java/com/ivan/walletservice/dto/TransactionRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.DecimalMin;
 import java.math.BigDecimal;
 
 public record TransactionRequest(
-        String playerLogin,
         @DecimalMin(message = "Amount must not less than 0.0!", value = "0.0", inclusive = false)
-        BigDecimal amount) {
+        BigDecimal amount,
+        String playerLogin) {
 }
